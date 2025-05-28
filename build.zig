2f9a1d4b55471e5da5cb595bce31e6142b1ce25a
rhodes-b <59537185+rhodes-b@users.noreply.github.com>
2025-05-28 04:51:28 -0500

    use best build practices

--- build.zig
+++ build.zig
@@ -4,15 +4,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_groove_gfx = b.addStaticLibrary(.{
-        .name = "groove-gfx",
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    b.installArtifact(lib_groove_gfx);
-
     const groove_gfx = b.addModule("groove_gfx", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
@@ -37,11 +28,15 @@ pub fn build(b: *std.Build) void {
     if (run_example) |example_to_run| {
         const example = b.addExecutable(.{
             .name = @tagName(example_to_run),
-            .root_source_file = b.path(b.fmt("examples/{s}.zig", .{@tagName(example_to_run)})),
-            .target = target,
-            .optimize = optimize,
+            .root_module = b.createModule(.{
+                .root_source_file = b.path(b.fmt("examples/{s}.zig", .{@tagName(example_to_run)})),
+                .imports = &.{
+                    .{ .name = "groove_gfx", .module = groove_gfx },
+                },
+                .target = target,
+                .optimize = optimize,
+            }),
         });
-        example.root_module.addImport("groove_gfx", groove_gfx);
         const example_run = b.addRunArtifact(example);
         example_step.dependOn(&example_run.step);
     }
